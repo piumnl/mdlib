1976cbbd564350cd4a8b756bc3418ad077e8ca6f
piumnl <piumnl@foxmail.com>
2018-04-26 23:41:56 +0800

    标题去除 .md 的后缀

--- src/main/java/cn/piumnl/mdlib/template/AbstractLibraryTemplate.java
+++ src/main/java/cn/piumnl/mdlib/template/AbstractLibraryTemplate.java
@@ -11,6 +11,8 @@ import cn.piumnl.mdlib.entity.Site;
  */
 public abstract class AbstractLibraryTemplate implements LibraryTemplate {
 
+    private static final String MD_SUFFIX = ".md";
+
     private Site site;
 
     private String ftlPath;
@@ -41,6 +43,10 @@ public abstract class AbstractLibraryTemplate implements LibraryTemplate {
     protected void initSiteModal(Map<String, Object> map) {
         map.put("uri", getSite().getUri());
         map.put("icon", getSite().getIcon());
-        map.put("title", getName());
+        map.put("title", getName().endsWith(MD_SUFFIX) ? noSuffixName() : getName());
+    }
+
+    private String noSuffixName() {
+        return getName().substring(0, getName().length() - MD_SUFFIX.length());
     }
 }
