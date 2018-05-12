78d2e7c0bfdbf9c657521299964a4955f19ae982
piumnl <piumnl@foxmail.com>
2018-05-13 00:43:17 +0800

    添加静态文件服务器功能

--- src/main/java/cn/piumnl/mdlib/Application.java
+++ src/main/java/cn/piumnl/mdlib/Application.java
@@ -2,17 +2,22 @@ package cn.piumnl.mdlib;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Properties;
 import java.util.logging.LogManager;
+import java.util.logging.Logger;
 
 import cn.piumnl.mdlib.entity.MdlibProperties;
 import cn.piumnl.mdlib.entity.Site;
+import cn.piumnl.mdlib.server.ServerContext;
 import cn.piumnl.mdlib.util.RefelectUtil;
 import cn.piumnl.mdlib.util.ResourceUtil;
+import cn.piumnl.mdlib.util.StringUtil;
 
 /**
  * @author piumnl
@@ -21,12 +26,39 @@ import cn.piumnl.mdlib.util.ResourceUtil;
  */
 public class Application {
 
+    /**
+     * 资源基路径
+     */
+    public static String outPath;
+
     public static void main(String[] args) throws IOException {
         initLogger();
-        processor();
+        Site processor = processor();
+        outPath = processor.getOut().toAbsolutePath().toString();
+
+        if (args.length > 0 && StringUtil.equals(args[0], "run")) {
+            Logger logger = Logger.getLogger(ServerContext.class.getName());
+            // 标准HTTP端口
+            int port = 20012;
+            ServerSocket server = new ServerSocket(port);
+            logger.info("静态资源服务器正在运行，端口为 " + port);
+
+            // 等待客户端的连接请求
+            // 获取请求的资源路径
+            // 读取文件返回
+
+            Socket client;
+            while (true) {
+                client = server.accept();
+                // ServerContext serverContext = ;
+                // serverContext.getResponse().done();
+                // Handler handler = new Handler(client, processor.getOut().toAbsolutePath().toString());
+                new Thread(new ServerContext(client)).start();
+            }
+        }
     }
 
-    private static void processor() throws IOException {
+    private static Site processor() throws IOException {
         // 读取配置文件 application.properties
         Path path = Paths.get("application.properties");
         Properties properties = ResourceUtil.loadProperties("/application.properties");
@@ -39,6 +71,8 @@ public class Application {
 
         Processor processor = new Processor(site);
         processor.processor();
+
+        return site;
     }
 
     private static void initLogger() {
