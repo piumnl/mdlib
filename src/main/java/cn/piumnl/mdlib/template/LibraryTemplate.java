05b14ed7cd8266f3fc8676db26b5ac354ba75808
piumnl <piumnl@foxmail.com>
2018-06-15 15:33:24 +0800

    添加注释

--- src/main/java/cn/piumnl/mdlib/template/LibraryTemplate.java
+++ src/main/java/cn/piumnl/mdlib/template/LibraryTemplate.java
@@ -11,11 +11,27 @@ import cn.piumnl.mdlib.entity.Site;
  */
 public interface LibraryTemplate {
 
+    /**
+     * 模板文件所需要的变量值
+     * @return 模板文件所需要的变量值
+     */
     Map<String, Object> dataModel();
 
+    /**
+     * 页面对应的模板位置
+     * @return 页面对应的模板位置
+     */
     String ftlPath();
 
+    /**
+     * 站点信息
+     * @return 站点信息类对象
+     */
     Site getSite();
 
+    /**
+     * 页面标题
+     * @return 页面标题
+     */
     String getName();
 }
