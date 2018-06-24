7542c8fee67f490f1381dfdcd09c4de2e8f41286
piumnl <piumnl@foxmail.com>
2018-06-24 10:30:21 +0800

    添加日志的配置处理 Log

--- src/main/java/cn/piumnl/mdlib/util/LoggerUtil.java
+++ src/main/java/cn/piumnl/mdlib/util/LoggerUtil.java
@@ -1,5 +1,6 @@
 package cn.piumnl.mdlib.util;
 
+import java.util.Objects;
 import java.util.logging.Logger;
 
 /**
@@ -9,10 +10,39 @@ import java.util.logging.Logger;
  */
 public interface LoggerUtil {
 
-    Logger MDLIB_LOGGER = Logger.getLogger("cn.piumnl.mdlib");
+    Log MDLIB_LOGGER = new Log(Logger.getLogger("cn.piumnl.mdlib"));
 
-    Logger PROCESSOR_LOGGER = Logger.getLogger("cn.piumnl.mdlib.processor");
+    Log PROCESSOR_LOGGER = new Log(Logger.getLogger("cn.piumnl.mdlib.processor"));
 
-    Logger SERVER_LOGGER = Logger.getLogger("cn.piumnl.mdlib.server");
+    Log SERVER_LOGGER = new Log(Logger.getLogger("cn.piumnl.mdlib.server"));
 
+    class Log {
+
+        private Logger logger;
+
+        public Log(Logger logger) {
+            Objects.requireNonNull(logger);
+            this.logger = logger;
+        }
+
+        public void trace(String msg, Object... data) {
+            logger.finest(StringUtil.format(msg, data));
+        }
+
+        public void debug(String msg, Object... data) {
+            logger.fine(StringUtil.format(msg, data));
+        }
+
+        public void info(String msg, Object... data) {
+            logger.info(StringUtil.format(msg, data));
+        }
+
+        public void warning(String msg, Object... data) {
+            logger.warning(StringUtil.format(msg, data));
+        }
+
+        public void error(String msg, Object... data) {
+            logger.severe(StringUtil.format(msg, data));
+        }
+    }
 }
