6f346a831615d99d5e112ef18833a750d310b7ba
piumnl <piumnl@foxmail.com>
2018-06-24 18:25:46 +0800

    Markdown 文档已完成实时刷新功能

--- src/main/java/cn/piumnl/mdlib/handler/MarkdownHandler.java
+++ src/main/java/cn/piumnl/mdlib/handler/MarkdownHandler.java
@@ -6,12 +6,13 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.Map;
 
 import cn.piumnl.mdlib.entity.Site;
 import cn.piumnl.mdlib.template.md.MarkdownTemplate;
 import cn.piumnl.mdlib.util.FileUtil;
 import cn.piumnl.mdlib.util.LoggerUtil;
-import cn.piumnl.mdlib.util.StringUtil;
 
 /**
  * @author piumnl
@@ -20,8 +21,29 @@ import cn.piumnl.mdlib.util.StringUtil;
  */
 public class MarkdownHandler implements Handler {
 
+    private static final MarkdownHandler HANDLER = new MarkdownHandler();
+
+    private Map<String, Long> fileInfo;
+
+    private MarkdownHandler() {
+    }
+
+    public static MarkdownHandler getInstance() {
+        return HANDLER;
+    }
+
+    @Override
+    public void refresh(Site site) throws Exception {
+        site.getLibraries()
+            .stream()
+            .flatMap(library -> library.getDir().stream())
+            .distinct()
+            .forEach(s -> renderMd(Paths.get(s).toAbsolutePath(), site));
+    }
+
     @Override
     public void process(Site site) {
+        fileInfo = new HashMap<>();
         site.getLibraries()
             .stream()
             .flatMap(library -> library.getDir().stream())
@@ -46,25 +68,28 @@ public class MarkdownHandler implements Handler {
 
         for (File subFile : subFiles) {
             if (subFile.isDirectory()) {
-                LoggerUtil.PROCESSOR_LOGGER.info(StringUtil.format("into dir: '{}'", subFile.getAbsolutePath()));
+                LoggerUtil.PROCESSOR_LOGGER.trace("进入目录： '{}'", subFile.getAbsolutePath());
 
                 renderMd(subFile, root, site);
             } else {
-                if (!subFile.getName().endsWith(".md")) {
-                    return;
+                Long lastModified = fileInfo.get(subFile.getAbsolutePath());
+                if (lastModified == null || lastModified < subFile.lastModified()) {
+                    if (!subFile.getName().endsWith(".md")) {
+                        return;
+                    }
+
+                    LoggerUtil.PROCESSOR_LOGGER.info("  渲染md文件： '{}'", Paths.get(subFile.getAbsolutePath()).normalize());
+
+                    String outPath = FileUtil.generatedOutPath(root, subFile.toPath(), site.getOut()) + ".html";
+                    Path resolve = site.getOut().resolve(outPath);
+                    FileUtil.createFile(resolve);
+
+                    String renderContent = FileUtil.render(new MarkdownTemplate(site, subFile));
+                    Files.write(resolve, renderContent.getBytes(StandardCharsets.UTF_8));
+
+                    fileInfo.put(subFile.getAbsolutePath(), subFile.lastModified());
+                    LoggerUtil.PROCESSOR_LOGGER.info("          => '{}'", Paths.get(outPath).normalize());
                 }
-
-                LoggerUtil.PROCESSOR_LOGGER.info(StringUtil.format("  render md: '{}'", Paths.get(subFile.getAbsolutePath()).normalize()));
-
-                String outPath = FileUtil.generatedOutPath(root, subFile.toPath(), site.getOut()) + ".html";
-                Path resolve = site.getOut().resolve(outPath);
-                FileUtil.createFile(resolve);
-
-                String renderContent = FileUtil.render(new MarkdownTemplate(site, subFile));
-                Files.write(resolve, renderContent.getBytes(StandardCharsets.UTF_8));
-
-                site.addMdFile(outPath);
-                LoggerUtil.PROCESSOR_LOGGER.info(StringUtil.format("          => '{}'", Paths.get(outPath).normalize()));
             }
         }
     }
